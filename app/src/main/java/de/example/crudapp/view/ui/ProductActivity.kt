bb82ad602387b800b5d1f105b6c0a33026be0f2d
Fahri MacBook <fahri-can@web.de>
2020-11-05 22:11:00 +0100

    Create new activity.

---
+++ app/src/main/java/de/example/crudapp/view/ui/ProductActivity.kt
@@ -0,0 +1,12 @@
+package de.example.crudapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.example.crudapp.R
+
+class ProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
