2fb6431c44bf246e7c20a300acefcb8a6a32b298
Fahri MacBook <fahri-can@web.de>
2020-11-05 22:12:40 +0100

    Set actionbar title.

--- app/src/main/java/de/example/crudapp/view/ui/ProductActivity.kt
+++ app/src/main/java/de/example/crudapp/view/ui/ProductActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import de.example.crudapp.R
 
 class ProductActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
+        supportActionBar?.title = "Create a new product"
     }
+
 }
