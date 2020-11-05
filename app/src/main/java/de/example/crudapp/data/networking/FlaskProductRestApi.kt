c86757d5a978e5e444ab6eed518eb6ca99c6b43c
Fahri MacBook <fahri-can@web.de>
2020-11-05 22:47:53 +0100

    Create method for POST request.

--- app/src/main/java/de/example/crudapp/data/networking/FlaskProductRestApi.kt
+++ app/src/main/java/de/example/crudapp/data/networking/FlaskProductRestApi.kt
@@ -1,11 +1,17 @@
 package de.example.crudapp.data.networking
 
+import de.example.crudapp.model.Product
 import de.example.crudapp.model.ProductsResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface FlaskProductRestApi {
 
     @GET("products")
     suspend fun getProducts(): ProductsResponse
+
+    @POST("product")
+    suspend fun createProduct(@Body product: Product): Product
 
 }
